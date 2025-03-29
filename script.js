14070b56929c77b61e52c8f397c0c52e80434af3
adriantech-beep <Adhrianne29@gmail.com>
2025-03-29 13:14:05 +0800

    added smooth scrolling on pages

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ nav.addEventListener("click", function (e) {
 faXmark.addEventListener("click", () =>
   navModalMobile.classList.add("modal-hidden")
 );
+
+document.querySelector(".nav-links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+document
+  .querySelector(".nav-links__mobile")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("nav-link-mobile")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
